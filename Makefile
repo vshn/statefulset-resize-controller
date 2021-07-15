d9b7717f77759152bc100fd999a6415b9bc1bedb
Fabian Fischer <glrf@users.noreply.github.com>
2021-07-15 13:13:08 +0200

    Add operator-sdk boilerplate

--- Makefile
+++ Makefile
@@ -1,23 +1,36 @@
-
 all: fmt vet build
 
 .PHONY: build
 build: 
 	CGO_ENABLED=0 go build
 
+
+run: fmt vet ## Run against the configured Kubernetes cluster in ~/.kube/config
+	go run ./main.go
+
 .PHONY: test
-test:
+test: fmt vet ## Run tests 
 	go test ./... -coverprofile cover.out
 
 .PHONY: fmt
-fmt: ## Run go fmt against code
+fmt: generate ## Run go fmt against code
 	go fmt ./...
 
 .PHONY: vet
-vet: ## Run go vet against code
+vet: generate ## Run go vet against code
 	go vet ./...
 
 .PHONY: lint
 lint: fmt vet ## Invokes the fmt and vet targets
 	@echo 'Check for uncommitted changes ...'
 	git diff --exit-code
+
+.PHONY: help
+help: ## Display this help.
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+.PHONY: generate
+generate:  ## Generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
+	# TODO(glrf) move actual code generation to go generate
+	go generate
+
---
+++ controllers/statefulset_controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"context"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// StatefulSetReconciler reconciles a StatefulSet object
+type StatefulSetReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=statefulsets/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=apps,resources=statefulsets/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the StatefulSet object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+func (r *StatefulSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *StatefulSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appsv1.StatefulSet{}).
+		Complete(r)
+}
